4cfa6abd2cb8cd4d434b0991b1eb89d7006bea04
Spencer Pearson <spencer.pearson.816@outlook.com>
2020-11-01 16:24:34 -0600

    Build temperature converter

---
+++ CSF1Homework/TempConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class TempConverter
+    {
+        static void Main()
+        {
+            Console.WriteLine("Temperature Conversion Tool Version 1.0");
+            Console.Title = "Temperature Conversion Tool";
+            Console.WriteLine("Would you like to convert from F)ahrenheit or from C)elsius?");
+            string userChoice = Console.ReadLine().ToUpper();
+            if (userChoice == "C" || userChoice == "Celsius")
+            {
+                Console.Write("Enter the temperature in degrees Celsuis: ");
+                decimal tempCelsius = Convert.ToDecimal(Console.ReadLine());
+                decimal tempFahrenheit = ((tempCelsius * 9 / 5) + 32);
+                Console.WriteLine($"{tempCelsius}°C converts to {tempFahrenheit:0.#}°F. ");
+            }
+            else if (userChoice == "F" || userChoice == "Fahrenheit")
+            {
+                Console.Write("Enter the temperature in degrees Fahrenheit: ");
+                decimal tempF = Convert.ToDecimal(Console.ReadLine());
+                decimal tempC = ((tempF - 32) * 5 / 9);
+                Console.WriteLine($"{tempF}°F converts to {tempC:0.#}°C. ");
+            }
+            else
+            {
+                Console.WriteLine("That was not a valid option. Please run again and select F or C.");
+            }
+        }    
+    }
+}
